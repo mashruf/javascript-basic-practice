c531edd45df4d12487e8e0d8f2c6e42753b15ceb
Mashruf Rohon <mashrufrohon@gmail.com>
2025-05-09 17:12:57 +0600

    new problem added

---
+++ printSentenceUsingVariable.js
@@ -0,0 +1,4 @@
+let name = "Mashruf";
+let age = 28;
+
+console.log("My name is "+name+" and I am "+age+" years old");
---
+++ stringToNumberNumberToString.js
@@ -0,0 +1,10 @@
+//string to number
+let str = "123";
+let toNum = Number(str);
+console.log("String to number is "+toNum);
+
+//number to string
+let num = 456;
+let toString = String(num);
+console.log("Number to string is "+toString);
+
